cf29a8efd83a2ef0d6e21ed6ff64c457bf24ea5d
Jimmy Givans <owuorjimmygivansomondi@gmail.com>
2023-02-10 15:05:24 +0300

    Fruit: Implement getters for member variables

--- Fruit.cpp
+++ Fruit.cpp
@@ -5,3 +5,22 @@ Fruit::Fruit(int x, int y) noexcept : m_position(x, y), m_shape(8)
     m_shape.setFillColor(sf::Color::Green);
 }
 
+sf::Vector2i const& Fruit::getPosition() const& noexcept
+{
+    return m_position;
+}
+
+sf::Vector2i Fruit::getPosition() && noexcept
+{
+    return std::move(m_position);
+}
+
+sf::CircleShape const& Fruit::getShape() const& noexcept
+{
+    return m_shape;
+}
+
+sf::CircleShape Fruit::getShape() && noexcept
+{
+    return std::move(m_shape);
+}
--- Fruit.hpp
+++ Fruit.hpp
@@ -15,6 +15,34 @@ public:
      */
     Fruit(int x, int y) noexcept;
 
+    /**
+     * @brief Get the position of the fruit
+     * 
+     * @return sf::Vector2i const& The position of the fruit as a const lvalue reference
+     */
+    sf::Vector2i const& getPosition() const& noexcept;
+    
+    /**
+     * @brief Get the position of the fruit
+     * 
+     * @return sf::Vector2i The position of the fruit as an rvalue
+     */
+    sf::Vector2i getPosition() && noexcept;
+
+    /**
+     * @brief Get the shape representing the fruit
+     * 
+     * @return sf::CircleShape const& The shape as a const lvalue reference
+     */
+    sf::CircleShape const& getShape() const& noexcept;
+    
+    /**
+     * @brief Get the shape representing the fruit
+     * 
+     * @return sf::CircleShape The shape as an rvalue reference
+     */
+    sf::CircleShape getShape() && noexcept;
+
 private:
     sf::Vector2i m_position {};
     sf::CircleShape m_shape {};
