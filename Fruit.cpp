cc067d8375ad68c4aee3519f049e324215b26871
Jimmy Givans <owuorjimmygivansomondi@gmail.com>
2023-02-10 14:57:19 +0300

    Set the radius and fill colour of the fruit circle

--- Fruit.cpp
+++ Fruit.cpp
@@ -1,6 +1,7 @@
 #include "Fruit.hpp"
 
-Fruit::Fruit(int x, int y) noexcept : m_position(x, y)
+Fruit::Fruit(int x, int y) noexcept : m_position(x, y), m_shape(8)
 {
+    m_shape.setFillColor(sf::Color::Green);
 }
 
