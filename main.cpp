d3a3472502d8694dc8ce685ee135c1105352bcd2
Jimmy Givans <owuorjimmygivansomondi@gmail.com>
2023-02-10 14:30:33 +0300

    Create and open a window and listen for events

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
 #include <cstdlib>
 
+#include <SFML/Graphics.hpp>
+#include <SFML/Window/Keyboard.hpp>
+
 int main()
 {
+    sf::RenderWindow window(sf::VideoMode(640, 480), "Snake");
+    
+    while (window.isOpen()) {
+        sf::Event event;
+
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+        }
+
+        window.clear();
+        window.display();
+    }
+
     return EXIT_SUCCESS;
 }
