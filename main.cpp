b43d5dbe506fc20824629e72673c68aba9fd70f9
Jimmy Givans <owuorjimmygivansomondi@gmail.com>
2023-02-23 17:25:41 +0300

    main: use Game class

--- main.cpp
+++ main.cpp
@@ -1,49 +1,12 @@
 #include <cstdlib>
 
-#include <SFML/Graphics.hpp>
-#include <SFML/Window/Keyboard.hpp>
-#include <SFML/System/Clock.hpp>
-#include <SFML/System/Sleep.hpp>
-#include <SFML/System/Time.hpp>
+#include "Game.hpp"
 
-#include "Common.hpp"
-#include "Snake.hpp"
-#include "Fruit.hpp"
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(Dimensions.width, Dimensions.height), "Snake");
-    Snake snake;
-    Fruit fruit;
-    
-    sf::Clock clock;
-    sf::Time elapsedTime;
-
-    static constexpr float timeStep = 1.0f / (BlockSize * 10); 
-
-    while (window.isOpen()) {
-        sf::Event event;
-
-        while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed) {
-                window.close();
-            }
-        }
-
-        if (elapsedTime >= sf::seconds(timeStep)) {
-            snake.setDirection();
-            snake.move();
-            snake.fitSnakeInGrid();
-            elapsedTime -= sf::seconds(timeStep);
-        }
-
-        window.clear();
-        drawFruit(window, fruit);
-        drawSnake(window, snake);
-        window.display();
-
-        elapsedTime += clock.restart();
-    }
+    Game game;
+    game.run();
 
     return EXIT_SUCCESS;
 }
