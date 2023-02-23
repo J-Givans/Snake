50ea8fa3c8e6ee0e745cb4b103aed44b98fc9f7d
Jimmy Givans <owuorjimmygivansomondi@gmail.com>
2023-02-23 18:54:32 +0300

    Game: handle collisions

--- src/Game.cpp
+++ src/Game.cpp
@@ -21,5 +21,15 @@ void Game::run()
 void Game::tick() noexcept
 {
     m_snake.move();
+
+    // If the snake eats the fruit, respawn the fruit elsewhere in the grid, and grow the snake
+    if (m_snake.getHead() == m_fruit.getPosition()) {
+        m_snake.grow();
+        m_fruit.respawn();
+    }
+
+    // If the snake collides with itself, shrink it
+    m_snake.handleCollisionWithSelf();
+
     m_snake.fitSnakeInGrid();
 }
