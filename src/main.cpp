92cc5f38cad18f5816a62fb08189c9152a1d454d
Jimmy Givans <owuorjimmygivansomondi@gmail.com>
2023-03-30 15:25:41 +0300

    Feat - render a fruit to the screen
    - Define a Fruit and its component types
    - Set the properties of the fruit components
    - Initialise the fruit and draw it to the screen

--- src/main.cpp
+++ src/main.cpp
@@ -2,14 +2,31 @@
 #include <SFML/Window/Window.hpp>
 #include <SFML/Window/Event.hpp>
 #include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/System/Vector2.hpp>
+#include <SFML/Graphics/CircleShape.hpp>
 
 #include <cstdlib>
 
 inline constexpr unsigned int BlockSize = 16;
 
+struct Fruit
+{
+    sf::Vector2i position;
+    sf::CircleShape shape;
+
+    Fruit()
+    {
+        auto const r = BlockSize / 2;
+        shape.setRadius(static_cast<float>(r));
+        shape.setFillColor(sf::Color::Red);
+        shape.setPosition(static_cast<float>(position.x * BlockSize), static_cast<float>(position.y * BlockSize));
+    }
+};
+
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(600, 400), "Classic Game Of Snake");
+    Fruit fruit;
 
     while (window.isOpen()) {
         sf::Event event;
@@ -21,6 +38,7 @@ int main()
         }
 
         window.clear();
+        window.draw(fruit.shape);
         window.display();
     }
 
