21f794e683cd1dd77935ae25b2a9547e01b135bb
Jimmy Givans <owuorjimmygivansomondi@gmail.com>
2023-03-30 15:14:19 +0300

    Feat - create a RenderWindow to render game components to the screen

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,27 @@
+#include <SFML/Window/VideoMode.hpp>
+#include <SFML/Window/Window.hpp>
+#include <SFML/Window/Event.hpp>
+#include <SFML/Graphics/RenderWindow.hpp>
+
 #include <cstdlib>
+
 
 int main()
 {
+    sf::RenderWindow window(sf::VideoMode(600, 400), "Classic Game Of Snake");
+
+    while (window.isOpen()) {
+        sf::Event event;
+        
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+        }
+
+        window.clear();
+        window.display();
+    }
+
     return EXIT_SUCCESS;
 }
