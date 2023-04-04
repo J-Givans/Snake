ace785fbceb74cf7cf84dc10d0e14d779f1585f6
Jimmy Givans <owuorjimmygivansomondi@gmail.com>
2023-04-04 19:17:50 +0300

    Refactor - mod the window size by BlockSize when setting distribution bounds
    This saves us from having to mod the generated numbers by BlockSize when setting the position

--- src/Fruit.cpp
+++ src/Fruit.cpp
@@ -7,9 +7,9 @@ namespace snake::fruit
 {
     Fruit::Fruit(sf::Vector2u const& winsize)
     {
-        static std::uniform_int_distribution<> dist(winsize.x, winsize.y);
-        position.x = dist(generator) % BlockSize;
-        position.y = dist(generator) % BlockSize;
+        static std::uniform_int_distribution<> dist(winsize.x % BlockSize, winsize.y % BlockSize);
+        position.x = dist(generator);
+        position.y = dist(generator);
 
         auto const radius = BlockSize / 2;
         shape.setRadius(radius);
