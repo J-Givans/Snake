0cb66ec685d3aea830e2c25951dfee3d704d7d62
Jimmy Givans <owuorjimmygivansomondi@gmail.com>
2023-04-04 21:39:41 +0300

    Refactor - move Borders class to separate files

---
+++ src/Borders.cpp
@@ -0,0 +1,34 @@
+#include "Borders.hpp"
+#include "Common.hpp"
+
+namespace snake::world
+{
+    Borders::Borders(sf::Vector2u const& winSize)
+    {
+        for (std::array<sf::RectangleShape, 4>::size_type i = 0; i < m_borders.size(); ++i) {
+            m_borders[i].setFillColor(sf::Color(150, 0, 0));
+
+            if ((i + 1) % 2 != 0) {
+                m_borders[i].setSize(sf::Vector2f(static_cast<float>(winSize.x), snake::BlockSize));
+            }
+            else {
+                m_borders[i].setSize(sf::Vector2f(snake::BlockSize, static_cast<float>(winSize.y)));
+            }
+
+            if (i < 2) {
+                m_borders[i].setPosition(0, 0);
+            }
+            else {
+                m_borders[i].setOrigin(m_borders[i].getSize());
+                m_borders[i].setPosition(sf::Vector2f(winSize));
+            }
+        }
+    }
+
+    void Borders::render(sf::RenderWindow& window) const
+    {
+        for (auto const& border : m_borders) {
+            window.draw(border);
+        }
+    }
+}
---
+++ src/Borders.hpp
@@ -0,0 +1,28 @@
+#ifndef BORDERS_HPP
+#define BORDERS_HPP
+
+#include <SFML/Graphics/RectangleShape.hpp>
+#include <SFML/Graphics/RenderWindow.hpp>
+
+#include <SFML/System/Vector2.hpp>
+#include <array>
+
+namespace snake::world
+{
+    class Borders
+    {
+    public:
+        /// \brief Constructor
+        /// \param[in] winsize The size of the window to which the borders will be rendered
+        explicit Borders(sf::Vector2u const& winsize);
+
+        /// \brief Render the borders to the screen
+        /// \param[in] The window in which the borders will be rendered
+        void render(sf::RenderWindow& window) const;
+
+    private:
+        std::array<sf::RectangleShape, 4> m_borders;
+    };
+}
+
+#endif
