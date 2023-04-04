397638bd2d45d5864d02aecfac5f69795114a63c
Jimmy Givans <owuorjimmygivansomondi@gmail.com>
2023-04-04 20:43:30 +0300

    Refactor - use respawn() to set position in the constructor

--- src/Fruit.cpp
+++ src/Fruit.cpp
@@ -7,14 +7,11 @@ namespace snake::fruit
 {
     Fruit::Fruit(sf::Vector2u const& winsize) : m_winsize { winsize }
     {
-        static std::uniform_int_distribution<> dist(winsize.x % BlockSize, winsize.y % BlockSize);
-        m_position.x = dist(generator);
-        m_position.y = dist(generator);
-
+        respawn();
+        
         auto const radius = BlockSize / 2;
         m_shape.setRadius(radius);
         m_shape.setFillColor(sf::Color::Yellow);
-        m_shape.setPosition(sf::Vector2f(m_position * BlockSize));
     }
 
     void Fruit::render(sf::RenderWindow& window) const
