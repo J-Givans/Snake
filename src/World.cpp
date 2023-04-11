df9808778c8940affefb4a6d11b5a7f19de6c08f
Jimmy Givans <owuorjimmygivansomondi@gmail.com>
2023-04-11 11:55:42 +0300

    Define the borders of the game world

---
+++ src/World.cpp
@@ -0,0 +1,8 @@
+#include "World.hpp"
+
+namespace snake::world
+{
+    World::World(sf::Vector2u const& winsize) : m_borders(winsize)
+    {
+    }
+}
---
+++ src/World.hpp
@@ -0,0 +1,19 @@
+#ifndef WORLD_HPP
+#define WORLD_HPP
+
+#include "Borders.hpp"
+#include <SFML/System/Vector2.hpp>
+
+namespace snake::world
+{
+    class World
+    {
+    public:
+        explicit World(sf::Vector2u const& winsize);
+
+    private:
+        Borders m_borders;
+    };
+}
+
+#endif
